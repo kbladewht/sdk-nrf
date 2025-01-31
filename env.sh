d0749a6ec19704c1b141a3f450aab98d90b4b444
kbladewht <w00110601111@163.com>
2025-01-31 21:26:45 +0800

    add as backup

---
+++ env.sh
@@ -0,0 +1,5 @@
+export PATH=/c/ncs/toolchains/2d382dcd92:/c/ncs/toolchains/2d382dcd92/mingw64/bin:/c/ncs/toolchains/2d382dcd92/bin:/c/ncs/toolchains/2d382dcd92/opt/bin:/c/ncs/toolchains/2d382dcd92/opt/bin/Scripts:/c/ncs/toolchains/2d382dcd92/opt/nanopb/generator-bin:/c/ncs/toolchains/2d382dcd92/opt/zephyr-sdk/arm-zephyr-eabi/bin:/c/ncs/toolchains/2d382dcd92/opt/zephyr-sdk/riscv64-zephyr-elf/bin:$PATH
+export PYTHONPATH=/c/ncs/toolchains/2d382dcd92/opt/bin:/c/ncs/toolchains/2d382dcd92/opt/bin/Lib:/c/ncs/toolchains/2d382dcd92/opt/bin/Lib/site-packages
+export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
+export ZEPHYR_SDK_INSTALL_DIR=/c/ncs/toolchains/2d382dcd92/opt/zephyr-sdk
+export ZEPHYR_BASE=/c/ncs/v2.8.0/zephyr
